9e14ecd8b2dca96a68f26ad4d7ce2181d52d4897
Study-CCC <1013409555@qq.com>
2019-08-10 21:44:52 +0800

    微信小程序

---
+++ shopping/pages/index/index.js
@@ -0,0 +1,11 @@
+const app=getApp()
+
+Page({
+  data:{
+    imgUrls:[
+      '/image/b1.jpg',
+      '/image/b2.jpg',
+      '/image/b3.jpg'
+    ]
+  }
+})
---
+++ shopping/pages/user/user.js
@@ -0,0 +1,103 @@
+// pages/user/user.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let self=this
+    wx.request({
+      url: 'http://www.gdfengshuo.com/api/wx/orders.txt',
+      success(res){
+        self.setData({
+          orders:res.data
+        })
+      }
+    })
+  },
+  payOrders() {
+    wx.requestPayment({
+      timeStamp: 'String1',
+      nonceStr: 'String2',
+      package: 'String3',
+      signType: 'MD5',
+      paySign: 'String4',
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function (res) {
+        wx.showModal({
+          title: '支付提示',
+          content: '<text>',
+          showCancel: false
+        })
+      }
+    })
+  },
+
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    wx.getStorage({
+      key: 'address',
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          hasAddress:true,
+          address:res.data
+        })
+      },
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
