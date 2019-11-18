363aaf5f653c16c77e18278f0e775def6100ffb4
Study-CCC <1013409555@qq.com>
2019-11-18 23:49:08 +0800

    手写webpack脚手架

---
+++ vue-cli-webpack/my-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      <router-link to="/login">登录</router-link>
+        <router-link to="/">首页</router-link>
+        <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang='' scoped>
+</style>
---
+++ vue-cli-webpack/my-app/src/pages/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="home">这是首页</div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang='' scoped>
+</style>
