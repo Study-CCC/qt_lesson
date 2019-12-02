440acc0ed395d13af0a1ac78618000d221866a67
Study-CCC <1013409555@qq.com>
2019-12-02 23:21:31 +0800

    vue加ts

---
+++ my-blog/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Nav></Nav>
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue,Component} from 'vue-property-decorator'
+// import Component from 'vue-class-component'
+import Nav from '@/components/nav.vue'
+
+@Component({
+  components: {
+    Nav,
+  },
+})
+
+export default class App extends Vue {
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
---
+++ my-blog/src/components/RegisterAndLogin.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-dialog title="登录" :visible="dialogVisible" @close="cancel">
+     <el-form label-width="80px">
+      <el-form-item label="邮箱">
+        <el-input v-model="params.email"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="params.password"></el-input>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component, Emit } from 'vue-property-decorator';
+@Component
+export default class RegisterAndLogin extends Vue {
+  @Prop({default: false}) private visible!: boolean;
+  private params: any = ({
+    email: '',
+    password: ''
+  })
+  get dialogVisible(): boolean {
+    console.log(this.visible);
+    return this.visible;
+  }
+  @Emit()
+  private cancel(): boolean {
+    return false;
+  }
+}
+</script>
+
+<style>
+
+</style>
