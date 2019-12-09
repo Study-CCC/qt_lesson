e7822fc570188b58b5ed7a18a2e197c85f1d1e65
Study-CCC <1013409555@qq.com>
2019-12-09 23:09:22 +0800

    面试题

--- 手写JS系列/节流/throttle.js
+++ 手写JS系列/节流/throttle.js
@@ -1,11 +1,11 @@
-const throttle = (fn,delay) =>{
-    let flag = true
-    return (...args)=>{
-        if(!flag) return
-        flag = false
-        setTimeout(()=>{
-            fn.apply(this,...args)  //防止fn方法里面的this指向改变
-            flag = true
-        },delay)
-    }
+const throttle = (fn, delay) => {
+  let flag = true;
+  return (...args) => {
+    if (!flag) return
+    flag = false
+    setTimeout(() => {
+      fn.apply(this, ...args)
+      flag = true
+    }, delay)
+  }
 }
--- 手写JS系列/防抖/debounce.js
+++ 手写JS系列/防抖/debounce.js
@@ -1,9 +1,9 @@
 const debounce = (fn, delay) => {
-    let timer = null
-    return (...args) => {
-        clearTimeout(timer)
-        timer = setTimeout(() => {
-            fn.apply(this,...args)
-        }, delay)
-    }
+  let timer = null;
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn.apply(this, ...args)
+    }, delay)
+  }
 }
